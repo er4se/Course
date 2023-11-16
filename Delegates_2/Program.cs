7a10a01ef74bab5d4173315e4ec5b88b48457255
erase <al.annko@mail.ru>
2023-11-17 00:48:40 +0300

    Added Delegates_2 homework

---
+++ Delegates_2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Delegates_2
+{
+    delegate int RandomInt();
+    delegate int RandomIntDelegate(params RandomInt[] randoms);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            RandomInt[] randomInts = new RandomInt[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                randomInts[i] = () =>
+                {
+                    Random rand = new Random();
+                    int random = rand.Next(0, 100);
+
+                    return random;
+                };
+            }
+
+            RandomIntDelegate anonymous = delegate (RandomInt[] randoms)
+            {
+                int count = 0; int sum = 0;
+                foreach (RandomInt rand in randoms)
+                {
+                    int temp = rand.Invoke();
+                    sum += temp; count++;
+                }
+
+                return sum / count;
+            };
+
+            Console.WriteLine(anonymous(randomInts));
+        }
+    }
+}
