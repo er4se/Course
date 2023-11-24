074b908eb80300709dc55edb464c176b1e61df6a
erase <al.annko@mail.ru>
2023-11-24 12:18:21 +0300

    Threads_1 fix

--- Threads1/Program.cs
+++ Threads1/Program.cs
@@ -3,36 +3,43 @@ using System.Threading;
 
 namespace Threads_1
 {
+                                                                                    //---------------------Программа не совершенна!---------------------//
+                                                                                    //Процессору требуется несколько секунд, чтобы запустить все потоки!//
+                                                                                    //------------------------------------------------------------------//
     class ChainClass
     {
-        object block = new object();
-
-        static Random random = new Random();
-        public void Chain(object coordinate)
+        object block = new object();                                                //Блокировщик потоков
+        static Random random = new Random();                                        //Рандом
+        public void Chain(object coordinate)                                        //Функция для вывода одной "капли дождя", получает на вход координату X
         {
-            int x = (int)coordinate;
-            int len = random.Next(3, 5);
-            int eternal_i = random.Next(0, Console.WindowHeight - 1);
-            int diff = 0;
+            int x = (int)coordinate;                                                //Координата X в плоскости консоли
+            int len = random.Next(3, 5);                                            //Случайная длина "капли" (от 3 до 5 в данном случае, при желании можно поменять)
+            int eternal_i = random.Next(0, Console.WindowHeight - 1);               //Генерация стартовой точки "капли дождя" по Y
+            int diff = 0;                                                           //Функция для сохранения положения хвоста "капли"
 
-            for (int i = eternal_i; i < (Console.WindowHeight - 1); i++)
+            for (int i = eternal_i; i < (Console.WindowHeight); i++)                //Цикл проходит консоль по Y
             {
-                lock (block)
+                lock (block)                                                        //Блокировка для потока
                 {
-                    int symbolCode = random.Next(0, 26);
-                    char symbol = (char)('a' + symbolCode);
+                    int symbolCode = random.Next(0, 26);                            //Генерация кода случайного символа
+                    char symbol = (char)('a' + symbolCode);                         //Перевод в char
 
-                    diff = i - len;
-                    if (diff < 0)
+                    diff = i - len;                                                 //Получение Y координаты для конца "капли"
+                    if (diff == 0)                                                  //Стирание конца "капли" в начале консоли (костыль)
                     {
-                        diff = len + diff;
+                        Console.ForegroundColor = Console.BackgroundColor;
+                        Program.WriteAt(symbol, x, 0);                              //WriteAt - пишет один символ по конкретным координатам (X, Y)
+                    }
+                    if (diff <= 0)                                                  //Стирание конца "капли" в конце консоли (костыль)
+                    {
+                        diff = (Console.WindowHeight ) + diff - 1;
                     }
 
-                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = Console.BackgroundColor;              //Стирание происходит путем присваивания шрифту цвета фона консоли
                     Program.WriteAt(symbol, x, diff);
 
-                    for(int j = 0; j < len; j++)
-                    {
+                    for(int j = 0; j < len; j++)                                    //Темно-зеленый хвост "капли", который занимает большую часть места
+                    {                                                               //Потенциально стоит оптимизировать
                         diff = i - j;
                         if (diff < 0)
                         {
@@ -42,15 +49,14 @@ namespace Threads_1
                         Program.WriteAt(symbol, x, diff);
                     }
 
-                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Green;                   //Зеленый хвост "капли" - занимает одну точку консоли
                     Program.WriteAt(symbol, x, i - 1);
 
-                    Console.ForegroundColor= ConsoleColor.White;
+                    Console.ForegroundColor= ConsoleColor.White;                    //Начало "капли" - занимает одну точку консоли
                     Program.WriteAt(symbol, x, i);
-                    //Thread.Sleep(100);
                 }
 
-                if (i == (Console.WindowHeight - 2))
+                if (i == (Console.WindowHeight - 1))                                //Цикл становиться бесконечным
                 {
                     i = 0;
                 }
@@ -61,35 +67,34 @@ namespace Threads_1
 
     class Program
     {
-        static int origRow;
+        static int origRow;                                                         //Координаты курсора, нужны для работы WriteAt
         static int origCol;
-        static public void WriteAt(char s, int x, int y)
+        static public void WriteAt(char s, int x, int y)                            //Пишет символ char s по координатам (X, Y)
         {
             try
             {
-                Console.SetCursorPosition(origCol + x, origRow + y);
-                Console.Write(s);
+                Console.SetCursorPosition(origCol + x, origRow + y);                //Переносит курсор на координаты (X, Y)
+                Console.Write(s);                                                   //Пишет символ
             }
-            catch (ArgumentOutOfRangeException e)
+            catch (ArgumentOutOfRangeException)
             {
                 Console.Clear();
-                Console.WriteLine(e.Message);
             }
         }
 
         static void Main(string[] args)
         {
             Console.Clear();
-            origRow = Console.CursorTop + 1;
+            origRow = Console.CursorTop;
             origCol = Console.CursorLeft;
 
             ChainClass chain = new ChainClass();
 
             ParameterizedThreadStart ChainThread = new ParameterizedThreadStart(chain.Chain);
-            
-            for(int i = 0; i < Console.WindowWidth; i++)
+
+            for (int i = 0; i < Console.WindowWidth; i++)
             {
-                new Thread(ChainThread).Start(i);
+                new Thread(ChainThread).Start(i);                                  //Генерация потоков для ChainClass.Chain(object c)
             }
         }
     }
